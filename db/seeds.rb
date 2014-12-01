1d1e9a9b9a9d69cf2e35dccb3e888963c76e3ac7
Sambath Prum <psb.sambath@gmail.com>
2014-12-01 14:22:07 -0600

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# create users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'helloworld'
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+# set first and last users to premium account
+first_user = users.first
+first_user.role = "premium"
+first_user.save
+
+last_user = users.last
+last_user.role = "premium"
+last_user.save
+
+# create wikis
+100.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    user: users.sample
+  )
+end
+
+puts "Seeding finished"
+puts "#{User.count} users created."
+puts "#{Wiki.count} wikis created."
